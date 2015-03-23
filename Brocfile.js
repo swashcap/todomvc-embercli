0e2bf977c0b256d2ee314665dc2425cf1c0d0058
Cory Reed <swashcap@gmail.com>
2015-03-22 23:36:22 -0700

    Add vendor assets, hard-coded markup.

--- Brocfile.js
+++ Brocfile.js
@@ -17,4 +17,7 @@ var app = new EmberApp();
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 
+app.import('vendor/style.css');
+app.import('vendor/bg.png');
+
 module.exports = app.toTree();
